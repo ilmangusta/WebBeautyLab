b3b72081a2ea0924fc0226e1439e017889c04c73
ilmangusta <lucabor25@gmail.com>
2025-02-06 23:22:47 +0100

    ok 2222

--- BeautyLab/src/index2.js
+++ BeautyLab/src/index2.js
@@ -8,7 +8,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// httpsjjjjjjj://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
